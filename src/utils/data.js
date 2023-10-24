a43d6ca62a476425aa90bcd1f6721a89f49b24e6
Dilbər Əmrahova <dilberamrahova1@gmail.com>
2023-10-24 14:07:16 +0000

    Update file data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,7 +1,5 @@
 import photo from "../assets/images/myimage.jpeg";
 import card1 from "../assets/images/card_1.png";
-// import code2 from "../assets/images/code3.jpg";
-// import ui2 from "../assets/images/ui2.jpg";
 import card3 from "../assets/images/card_3.png"
 
 
@@ -101,81 +99,3 @@ export const feedbackData = [
 
 
 
-
-
-
-// import Panel from "../components/Panel";
-// import Box from "../components/Box";
-// import TimeLine from "../components/TimeLine";
-// import Expertise from "../components/Expertise";
-// import Portfolio from "../components/Portfolio";
-// import Feedback from "../components/Feedback";
-// import "../assets/styles/Inner.scss";
-// import { useState } from "react";
-// // back-to-top button
-// import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
-// import { faBars } from "@fortawesome/free-solid-svg-icons";
-// import {
-//   timeLineData,
-//   experienceData,
-//   portfolioData,
-//   feedbackData,
-// } from "../utils/data";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import Button from "../components/Button";
-// import Address from "../components/Address";
-// const Inner = () => {
-//   const [isPanelOpen, setPanelOpen] = useState(true);
-//   const togglePanel = () => {
-//     setPanelOpen(!isPanelOpen);
-//   };
-//   // back-to-top button
-//   const handleUpButtonClick = () => {
-//     window.scrollTo({ top: 0, behavior: "smooth" });
-//   };
-//   return (
-//     <div className="main">
-//       <div className={`main__sidebar ${isPanelOpen ? "open" : "closed"}`}>
-//         <Panel></Panel>
-//       </div>
-//       <div className="main-content">
-//         <Button
-//           icon={<FontAwesomeIcon icon={faBars} onClick={togglePanel} />}
-//         />
-//         <div className="content">
-//           <Box
-//             id={"about"}
-//             title="About me"
-//             content="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque"
-//           ></Box>
-
-//           <Box id={"education"} title="Education">
-//             <TimeLine data={timeLineData} />
-//           </Box>
-
-//           <Box id={"experience"} title="Experience">
-//             <Expertise data={experienceData} />
-//           </Box>
-
-//           <Box id={"portfolio"} title="Portfolio">
-//             <Portfolio data={portfolioData} />
-//           </Box>
-
-//           <Box id={"contacts"} title="Contacts">
-//             <Address></Address>
-//           </Box>
-
-//           <Box id={"feedbacks"} title="Feedbacks">
-//             <Feedback data={feedbackData} />
-//           </Box>
-//           {/* Back-to-top button */}
-//           <div className="back-to-top-button" onClick={handleUpButtonClick}>
-//             <FontAwesomeIcon icon={faAngleUp} />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Inner;
